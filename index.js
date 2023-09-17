279e64a9ab2006233a1ded656e4b8cb46bcc02f1
ahmed-hasan92 <ahmed201130030@gmail.com>
2023-09-17 18:16:40 +0300

    ArrowTask

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ console.log(greet("Ahmed"));
 const sum = (param1, param2) => param1 + param2;
 console.log(sum(5, 2));
 // Q3) Write a simple arrow function that squares a number.
-const squareFun = (num) => num * 2;
+const squareFun = (num) => num * num;
 console.log(squareFun(8));
 // Q4) [🌶️🌶️] Create an arrow function that takes an array of numbers and returns a new array with each number squared.
 const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const returnSquar = (squarArray) => {
   let newSquarArray = [];
   for (let index = 0; index <= squarArray.length - 1; index++) {
-    newSquarArray.push(squarArray[index] * 2);
+    newSquarArray.push(squarArray[index] * [index]);
   }
   console.log(newSquarArray);
 };
